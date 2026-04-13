4ca17bca0408717a04342dfb27d1ad39ee790f93
YHWINDOWS\chang <changyiherng2004@gmail.com>
2026-04-13 16:25:17 +0800

    Minor bug fixes

--- src/test/java/seedu/pharmatracker/command/ViewCommandTest.java
+++ src/test/java/seedu/pharmatracker/command/ViewCommandTest.java
@@ -14,7 +14,6 @@ import seedu.pharmatracker.ui.Ui;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- src/test/java/seedu/pharmatracker/parser/ParserTest.java
+++ src/test/java/seedu/pharmatracker/parser/ParserTest.java
@@ -1,6 +1,5 @@
 package seedu.pharmatracker.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
